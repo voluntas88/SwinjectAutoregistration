7808e23fb93697eec050b67a3761ea3420fda655
Volodymyr Rudenok <vlado.rudenok@gmail.com>
2018-06-01 14:51:21 +0200

    [Resolver] try to resolve Service with arguments

--- Sources/Resolver.swift
+++ Sources/Resolver.swift
@@ -18,15 +18,27 @@ extension Resolver {
     }
 
     func resolve<Service, Arg1>(argument  arg1: Arg1) -> Service? {
-       return (arg1 as? Service) ?? self.resolve(Service.self)
+       return (arg1 as? Service)
+        ?? self.resolve(Service.self)
+        ?? self.resolve(Service.self, argument: arg1)
     }
 
     func resolve<Service, Arg1, Arg2>(arguments  arg1: Arg1, _ arg2: Arg2) -> Service? {
-       return (arg1 as? Service) ?? (arg2 as? Service) ?? self.resolve(Service.self)
+       return (arg1 as? Service)
+        ?? (arg2 as? Service)
+        ?? self.resolve(Service.self)
+        ?? self.resolve(Service.self, argument: arg1)
+        ?? self.resolve(Service.self, arguments: arg1, arg2)
     }
 
     func resolve<Service, Arg1, Arg2, Arg3>(arguments  arg1: Arg1, _ arg2: Arg2, _ arg3: Arg3) -> Service? {
-       return (arg1 as? Service) ?? (arg2 as? Service) ?? (arg3 as? Service) ?? self.resolve(Service.self)
+       return (arg1 as? Service)
+        ?? (arg2 as? Service)
+        ?? (arg3 as? Service)
+        ?? self.resolve(Service.self)
+        ?? self.resolve(Service.self, argument: arg1)
+        ?? self.resolve(Service.self, arguments: arg1, arg2)
+        ?? self.resolve(Service.self, arguments: arg1, arg2, arg3)
     }
 }
 
